a711bce5d03f99fb86617e60d4d460f893651bd5
Philip Obiri <philipobiri3.1@gmail.com>
2023-09-29 02:27:54 +0000

    feat: Add a basic  Homepage Text Component

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+// import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <p className="text-bold font-semibold">Hello World</p>
+    </div>
+  )
+}
